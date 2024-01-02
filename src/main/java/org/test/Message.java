34cc61de4311c0f1010a27157dfed5607079484d
Mart Somermaa <mrts@users.noreply.github.com>
2024-01-03 00:05:37 +0200

    Add cluster support to CDI events with jakartaee-clustered-cdi-events

--- src/main/java/org/test/Message.java
+++ src/main/java/org/test/Message.java
@@ -2,9 +2,13 @@ package org.test;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.clustercdievents.cdievents.Clustered;
 
+@Clustered
 @Data
 @AllArgsConstructor
-class Message {
+@NoArgsConstructor
+public class Message {
     private String text;
 }
